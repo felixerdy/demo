652d01463f8c76b9e11969a5566a0dbf3c6bbed5
felixerdy <felix.erdmann@uni-muenster.de>
2019-10-17 11:13:57 +0200

    Simple webserver with mongoDB connection

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+
+const User = mongoose.model('User', { name: String, password: String });
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.get('/users', function (req, res) {
+    User.find(function (err, users) {
+        if (err) return console.error(err);
+        res.send(users)
+    })
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.post('/user', function (req, res) {
+    const user = new User(req.body)
+    user.save().then(function (result) {
+        res.send(result)
+    }).catch(function (err) {
+        response.status(500).send(error);
+    })
+
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
